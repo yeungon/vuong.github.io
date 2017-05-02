307d434cf33888e2adebad394dfb13f6584b0b1d
Vuong Nguyen <yeungon@gmail.com>
2017-05-02 20:07:17 +1200

    Update mysqlconnect.php

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -20,5 +20,12 @@ echo htmlentities($row['_message']);
 
 // source: http://php.net/manual/en/mysqlinfo.api.choosing.php
 // more: http://php.net/manual/en/mysqli.quickstart.prepared-statements.php
+
+// Recommended API
+
+//It is recommended to use either the mysqli or PDO_MySQL extensions.
+// It is not recommended to use the old mysql extension for new development, as it was deprecated in PHP 5.5.0 and was removed in PHP 7. 
+// A detailed feature comparison matrix is provided below. The overall performance of all three extensions is considered to be about the same. 
+// Although the performance of the extension contributes only a fraction of the total run time of a PHP web request. Often, the impact is as low as 0.1%.
 ?>
 

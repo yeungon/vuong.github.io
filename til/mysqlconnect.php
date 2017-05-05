0d2e644b32555d7d31bae33c5d8f11108e7c7e9f
Vuong Nguyen <yeungon@gmail.com>
2017-05-05 20:50:43 +1200

    Update mysqlconnect.php

--- til/mysqlconnect.php
+++ til/mysqlconnect.php
@@ -1,4 +1,32 @@
 <?php
+/* 6 steps to work with mysql and php VUONG MAY 2017 -SELF-TAUGHT
+
+https://www.qhonline.edu.vn/video-traininge0a0bc94e5z53.html
+
+1) connect
+- create a variable 
+$a = mysql_connect ();
+2) select a database 
+- mysql_select_db ()
+3) querry
+- create another variable 
+$b= mysql_querry ();
+
+4) count the number of record (from the variable created in the previous step)
+mysql_num_row ($b);
+
+5) fetching data to php: 2 ways
+- mysql_fetch_array ();
+- mysql-fetch_assoc ();
+
+6) close mysql
+mysql_close ($a); close the previous variable
+
+*/
+
+
+
+
 // mysqli
 $mysqli = new mysqli("example.com", "user", "password", "database");
 $result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");

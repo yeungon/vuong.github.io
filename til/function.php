9c60430bff84f820c8a3bc6b30cecde024181ca6
Vuong Nguyen <yeungon@gmail.com>
2017-05-06 17:06:02 +1200

    Update function.php

--- til/function.php
+++ til/function.php
@@ -27,10 +27,79 @@ nên nó được ứng dụng vào việc tạo ra các ứng dụng autoloadin
 nếu chúng ta sử dụng nhiều hơn 1 lần include (require) thì dùng __autoload() sẽ nhanh hơn, 
 còn nếu chỉ include (require) 1 lần thì __autoload() chậm hơn 1 tí nhưng không đáng kể. 
 
-
 */
 
+//Thông thường, để khởi tạo 2 class trên trong file index.php thì chúng ta phải include từng file class như sau:
 
+<?php 
+//Nhúng file ConNguoi
+include_once 'NguoiLon.php';
+//Nhúng file ConNguoi
+include_once 'TreCon.php';
+
+//Khởi tạo 2 class
+$nguoilon = new NguoiLon();
+//Kết Quả: Class NguoiLon
+$trecon = new TreCon();
+//Kết Quả: Class TreCon
+
+// +Nhưng nếu chúng ta sử dụng  hàm __autoload() thì sẽ chỉ phải khai báo ngắn gọn như sau:
+
+<?php 
+//khai báo hàm __autoload
+function __autoload($className)
+{
+    //kiểm tra xem file tồn tại không
+    if(file_exists($className . '.php')){
+        //Nếu tồn tại thì nhúng file vào.
+        include_once $className . '.php';
+    }
+}
+//Khởi tạo 2 class
+$nguoilon = new NguoiLon();
+//Kết Quả: Class NguoiLon
+$trecon = new TreCon();
+//Kết Quả: Class TreCon
+
+
+/* Autoload khi gọi phương thức tĩnh.
+
+-Và hàm __autoload() này cũng được gọi khi chúng ta gọi phương thức tĩnh không xác định.
+
+VD: Chúng ta có 2 file ConNguoi.php và một file index.php ở cùng thư mục.
+
++File ConNguoi.php, chứa class ConNguoi có nội dung như sau:
+
+*/
+<?php 
+class ConNguoi
+{
+    private static $name = 'ConNguoi';
+    public function __construct()
+    {
+        
+    }
+    public static function getName()
+    {
+    	echo static::$name;
+    }
+
+}
+//File index.php, có nội dung như sau:
+
+<?php 
+//khai báo hàm __autoload
+function __autoload($className)
+{
+    //kiểm tra xem file tồn tại không
+    if(file_exists($className . '.php')){
+        //Nếu tồn tại thì nhúng file vào.
+        include_once $className . '.php';
+    }
+}
+ConNguoi::getName();
+
+//Kết Quả: ConNguoi
 
 ?>
 

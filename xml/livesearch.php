60fd2177d6a75c2cd3ef0bcec20f50a7cf337fe2
Vuong Nguyen <yeungon@gmail.com>
2017-05-09 17:30:31 +1200

    Add files via upload

---
+++ xml/livesearch.php
@@ -0,0 +1,53 @@
+<?php
+$xmlDoc=new DOMDocument();
+$xmlDoc->load("links.xml");
+
+$x=$xmlDoc->getElementsByTagName('id');
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('title');
+    $z=$x->item($i)->getElementsByTagName('content');
+    if ($y->item(0)->nodeType==1) {
+
+
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint= $z->item(0)->childNodes->item(0)->nodeValue;
+          $y->item(0)->childNodes->item(0)->nodeValue ;
+        } else {
+          $hint=$hint;
+          $z->item(0)->childNodes->item(0)->nodeValue;
+
+          $y->item(0)->childNodes->item(0)->nodeValue ;
+        }
+      }
+    }
+  }
+}
+
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="Không tìm thấy";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
+
+/*If there is any text sent from the JavaScript (strlen($q) > 0), the following happens:
+
+Load an XML file into a new XML DOM object
+Loop through all <title> elements to find matches from the text sent from the JavaScript
+Sets the correct url and title in the "$response" variable. If more than one match is found, all matches are added to the variable
+If no matches are found, the $response variable is set to "no suggestion"
+*/
+?>

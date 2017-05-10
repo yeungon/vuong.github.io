aa3b1c5acf47b85469aa187ec9c24118be841b0b
Vuong Nguyen <yeungon@gmail.com>
2017-05-11 00:26:36 +1200

    Update script.js

--- dict/script.js
+++ dict/script.js
@@ -4,8 +4,7 @@ $('#search').keyup(function() {
 	$.getJSON('content_final.json', function(data) {
 		var output = '<ul class="searchresults">';
 		$.each(data, function(key, val) {
-			if ((val.word.search(myExp) != -1) ||
-			(val.content.search(myExp) != -1)) {
+			if (val.word.search(myExp) != -1) {
 				output += '<li>';
 				output += '<h2>'+ val.word +'</h2>';
 				//output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ val.name +'" />';
